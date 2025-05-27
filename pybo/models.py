7a90c9ddaa2283693fecdcdb704518a91a615508
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-05-27 12:46:46 +0900

    FEAT: beans 모델 추가

--- pybo/models.py
+++ pybo/models.py
@@ -30,3 +30,13 @@ class Comment(models.Model):
     modify_date = models.DateTimeField(null=True, blank=True)
     question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
+
+
+class Beans(models.Model):
+    name = models.CharField(max_length=200)
+    roster = models.CharField(max_length=200)
+    roast = models.CharField(max_length=20, null=True)
+    origin = models.CharField(max_length=100)
+    price = models.FloatField()
+    rating = models.IntegerField()
+    review = models.TextField()
