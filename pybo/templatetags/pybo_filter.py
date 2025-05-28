69f8660d073f4e3e6c0dd0848cd2b605a69de96e
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-05-28 15:21:53 +0900

    FEAT: markdown 기능 추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,7 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
+
 
 register = template.Library()
 
@@ -6,3 +9,10 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+@register.filter
+def mark(value):
+    """입력 값을 HTML 문자열로 변환하는 함수"""
+    extentions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extentions))
