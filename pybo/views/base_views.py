db337bac1d09749df635fddadc8bce6a31de21b8
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-05-29 11:01:33 +0900

    FEAT: 검색 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
@@ -11,15 +12,25 @@ def index(request):
     """
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
+    keyword = request.GET.get('kw', '')  # 키워드
 
     # 조회
     question_list = QuestionService.get_all_questions()
+
+    if keyword:
+        question_list = question_list.filter(
+            Q(subject__icontains=keyword) |
+            Q(content__icontains=keyword) |
+            Q(answer__content__icontains=keyword) |
+            Q(author__username__icontains=keyword) |
+            Q(answer__author__username__icontains=keyword)
+        ).distinct()
 
     # 페이징처리
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': keyword}
     return render(request, 'pybo/question_list.html', context)
 
 
