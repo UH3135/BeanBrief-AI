68407cf7f3fd3989bdf1d85376aef3cd2847c6ec
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-05-29 11:52:37 +0900

    REFACT: 질문, 답변, 댓글 모델 각각 분류
    - Question, Answer, Comment 모델을 각각의 파일에서 생성
    - __init__.py 파일에 모델들 포함

---
+++ pybo/models/comment.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+from .question import Question
+from .answer import Answer
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
