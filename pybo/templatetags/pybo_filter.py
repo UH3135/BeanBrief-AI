e096be0ab296f9c2bcbed65632289844c16df1d4
sentryc0201 <yc020102@gmail.com>
2025-06-14 04:02:58 +0900

    Request를 이용한 파일 업로드 기능 추가

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -2,6 +2,7 @@ import markdown
 import markdown
 from django import template
 from django.utils.safestring import mark_safe
+import os
 
 
 register = template.Library()
@@ -17,3 +18,8 @@ def mark(value):
     """입력 값을 HTML 문자열로 변환하는 함수"""
     extentions = ["nl2br", "fenced_code"]
     return mark_safe(markdown.markdown(value, extensions=extentions))
+
+@register.filter
+def basename(value):
+    """파일 경로에서 파일명만 반환"""
+    return os.path.basename(value)
